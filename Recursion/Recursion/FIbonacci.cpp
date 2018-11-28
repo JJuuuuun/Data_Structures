07ce9f72cd274d126e87f838bc7645a47127bb48
JJuuuuun <34728086+JJuuuuun@users.noreply.github.com>
2018-11-29 00:12:15 +0900

    Fibonacci.cpp

---
+++ Recursion/Recursion/FIbonacci.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int Fibo(int n)
+{
+
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else
+		return Fibo(n - 1) + Fibo(n - 2);
+	
+}
+
+int main()
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d ", Fibo(i));
+
+	return 0;
+
+}
